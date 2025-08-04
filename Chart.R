3ab84d4ab3bef3e1828ddef5f1724beb2e976076
Aristotle Anastasiadis <aristotle.anastasiadis@esc.vic.gov.au>
2025-08-04 10:10:19 +1000

    Ready for training

---
+++ Chart.R
@@ -0,0 +1,15 @@
+#---------Packages---------
+library(ggplot2)
+
+#-----------Data-----------
+data <- read.csv("Data/Fuel-mix.csv")
+data$Fuel.Type <- factor(data$Fuel.Type , levels=c("Battery","Wind","Solar","Hydro","Gas","Brown coal"))
+data$DateTime <- as.POSIXct(data$DateTime, format = "%d/%m/%Y %H:%M")
+
+#----------Chart----------
+ggplot(data, aes(x=DateTime, y=Supply, fill=Fuel.Type)) +
+  geom_area() +
+  scale_fill_manual(values=c("#CC00FF","#92D050","yellow","#93DAF7","#009999","#996633"))+
+  labs(title="Hourly fuel mix", subtitle="Victoria - 2 August 2025")+
+  theme_minimal()+
+  theme(legend.position="bottom")
--- Code.R
+++
@@ -1,3 +0,0 @@
-# Code written using base R
-df <- data.frame()
-df$column3 <- df$column1 + df$column2
